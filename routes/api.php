51f8276a8479cdbd4604a2b85b1f8991a0625e9a
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-01-07 14:15:05 -0300

    Adicionando rotas novas para pokemons

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PokemonController;
 use App\Http\Controllers\TreinadorController;
 use App\Models\Treinador;
 use Illuminate\Http\Request;
@@ -10,3 +11,10 @@ Route::get('treinadores/{id}', [TreinadorController::class, 'show']);
 Route::post('treinadores', [TreinadorController::class,'store']);
 Route::put('treinadores/{id}', [TreinadorController::class,'update']);
 Route::delete('treinadores/{id}', [TreinadorController::class,'destroy']);
+
+#Pokemon Rotas
+Route::get('pokemons', [PokemonController::class, 'index']);
+Route::get('pokemons/{id}', [PokemonController::class, 'show']);
+Route::post('pokemons',[PokemonController::class, 'store']);
+Route::put('pokemons/{id}', [PokemonController::class, 'update']);
+Route::delete('pokemons/{id}', [PokemonController::class, 'destroy']);
