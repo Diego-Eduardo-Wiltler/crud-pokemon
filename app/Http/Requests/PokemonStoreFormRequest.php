e0ac0dd797383f404bf93754c30d30b55d0f0523
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-01-24 21:02:57 -0300

    Adicionando requests

---
+++ app/Http/Requests/PokemonStoreFormRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PokemonStoreFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:255',
+            'ataque' => 'required|int|min:5',
+            'defesa' => 'required|int|min:5',
+            'vida' =>'required|int|min:0',
+            'vida_atual' =>'required|int|min:50',
+            'tipo' =>'required|string|max:255',
+            'peso' =>'required|string|max:255',
+            'localizacao' =>'required|string|max:255',
+            'shiny' => 'required|boolean',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nome.required' => 'O nome é obrigatório.',
+            'ataque.required' => 'O ataque é obrigatório.',
+            'ataque.integer' => 'O ataque deve ser um número inteiro.',
+            'ataque.min' => 'O ataque não pode ser menor que 5.',
+            'defesa.required' => 'A defesa é obrigatória.',
+            'defesa.integer' => 'A defesa deve ser um número inteiro.',
+            'defesa.min' => 'A defesa não pode ser menor que 5.',
+            'vida.required' => 'A vida é obrigatória.',
+            'vida.integer' => 'A vida deve ser um número inteiro.',
+            'vida.min' => 'A vida não pode ser menor negativa.',
+            'vida_atual.required' => 'A vida atual é obrigatória.',
+            'vida_atual.integer' => 'A vida_atual deve ser um número inteiro.',
+            'vida_atual.min' => 'A vida_atual não pode ser menor que 50.',
+            'tipo.required' => 'O tipo é obrigatório.',
+            'regiao.required' => 'A região é obrigatória.',
+            'peso.required' => 'O peso é obrigatório.',
+            'localizacao.required' => 'A localização é obrigatória.',
+            'shiny.required' => 'O campo shiny é obrigatório.',
+            'shiny.boolean' => 'O campo shiny deve ser verdadeiro ou falso.',
+        ];
+    }
+
+}
---
+++ app/Http/Requests/TreinadorStoreFormRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TreinadorStoreFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'regiao' => 'required|string|max:255',
+            'tipo_favorito' => 'required|string|max:255',
+            'idade' => 'required|integer|min:0',
+            'pokemon_id' => 'nullable|integer|exists:pokemons,id',
+        ];
+
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.required' => 'O nome é obrigatório.',
+            'email.required' => 'O e-mail é obrigatório.',
+            'regiao.required' => 'A região é obrigatória.',
+            'tipo_favorito.required' => 'O tipo favorito é obrigatório.',
+            'idade.required' => 'A idade é obrigatória.',
+            'idade.integer' => 'A idade deve ser um número inteiro.',
+            'idade.min' => 'A idade não pode ser negativa.',
+            'pokemon_id.integer' => 'O ID do Pokémon deve ser um número inteiro.',
+            'pokemon_id.exists' => 'O ID do Pokémon informado não existe.',
+        ];
+    }
+
+}
