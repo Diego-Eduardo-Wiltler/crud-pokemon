0dcb0bfef254d9080493817802228c723cb77370
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-01-30 20:13:22 -0300

    Refatorando resources

--- app/Http/Resources/PokemonBatalhaResource.php
+++ app/Http/Resources/PokemonBatalhaResource.php
@@ -7,13 +7,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PokemonBatalhaResource extends JsonResource
 {
-    private $message;
 
-    public function __construct($resource, $message = null)
-    {
-        parent::__construct($resource);
-        $this->message = $message;
-    }
     /**
      * Transform the resource into an array.
      *
@@ -22,7 +16,6 @@ class PokemonBatalhaResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'message' => $this->message,
             'nome' => $this->nome,
             'tipo' => $this->tipo,
             'localizacao' => $this->localizacao,
--- app/Http/Resources/PokemonRoundBattleResource.php
+++ app/Http/Resources/PokemonRoundBattleResource.php
@@ -15,25 +15,11 @@ class PokemonRoundBattleResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'messages' => [
-                "{$this->resource['pokemonATK']->nome} atacou com {$this->resource['pokemonATK']->ataque} de dano",
-                "{$this->resource['pokemonDEF']->nome} {$this->resource['defesaTexto']}",
-                "{$this->resource['pokemonATK']->nome} causou {$this->resource['danoCausado']} de dano",
-                "{$this->resource['pokemonDEF']->nome} ainda se mantém na luta",
-                "{$this->resource['pokemonDEF']->nome} ainda possui {$this->resource['pokemonDEF']->vida} pontos de vida",
-            ],
-            'data' => [
-                'pokemon_atk' => [
-                    'id' => $this->resource['pokemonATK']->id,
-                    'nome' => $this->resource['pokemonATK']->nome,
-                    'ataque' => $this->resource['pokemonATK']->ataque,
-                ],
-                'pokemon_def' => [
-                    'id' => $this->resource['pokemonDEF']->id,
-                    'nome' => $this->resource['pokemonDEF']->nome,
-                    'defesa' => $this->resource['pokemonDEF']->defesa,
-                ],
-            ],
-        ];
+                'id' => $this->id,
+                'nome' => $this->nome,
+                'ataque' => $this->ataque ?? null,
+                'defesa' => $this->defesa ?? null,
+            ];
+
     }
 }
