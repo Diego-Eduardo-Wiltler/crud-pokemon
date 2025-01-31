6c2bea15d2317ccbd5ac14480b421247eff3cf0b
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-01-31 10:19:54 -0300

    Adicionando comentários ao código

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -21,6 +21,25 @@ class PokemonController extends Controller
         $this->pokemonService = $pokemonService;
     }
 
+
+    /**
+     * GET /pokemons
+     *
+     * Retorna lista de pokemons cadastrados
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: array<PokemonResource>
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
+
     public function index(): JsonResponse
     {
         $result = $this->pokemonService->getPokemons();
@@ -32,6 +51,23 @@ class PokemonController extends Controller
         }
         return $this->errorResponse($result['message']);
     }
+
+    /**
+     * GET /pokemons/{id}
+     *
+     * Retorna um unico pokemon pelo id
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *  @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
 
     public function show($id): JsonResponse
     {
@@ -45,6 +81,37 @@ class PokemonController extends Controller
 
         return $this->errorResponse($result['message']);
     }
+
+    /**
+     * POST /pokemons
+     *
+     * Cadastra um novo pokemon
+     *
+     * @bodyParam nome string required Nome do pokemon. Example: Caterpie
+     * @bodyParam ataque int required Valor de ataque do pokemon. Example: 5
+     * @bodyParam defesa int required Valor de defesa do pokemon. Example: 5
+     * @bodyParam vida int required Valor total de vida do pokemon. Example: 45
+     * @bodyParam vida_atual int required Valor atual de vida do pokemon. Example: 50
+     * @bodyParam tipo string required Tipo do pokemon. Example: Inseto
+     * @bodyParam peso string required Peso do pokemon. Example: 2.9Kg
+     * @bodyParam localizacao string required Localização onde o pokemon pode ser encontrado. Example: Floresta Selvagem - Grama Alta
+     * @bodyParam shiny int required Indica se o pokemon é shiny (0 para não, 1 para sim). Example: 0
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *
+     * @param PokemonStoreFormRequest $request Requisição contendo os dados do pokemon.
+     * @return JsonResponse
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
 
     public function store(PokemonStoreFormRequest $request): JsonResponse
     {
@@ -61,7 +128,29 @@ class PokemonController extends Controller
         return $this->errorResponse($request['message']);
     }
 
-
+     /**
+     * POST pokemons/battle
+     *
+     * Realiza a luta completa de dois pokemons
+     *
+     * @bodyParam pokemon:id1 int required id do pokemon. Example: 1
+     * @bodyParam pokemon:id2 int required id do pokemon. Example: 2
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *
+     * @param Request $request Requisição contendo os dados do pokemon.
+     * @return JsonResponse
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
 
     public function storeBattle(Request $request): JsonResponse
     {
@@ -78,6 +167,32 @@ class PokemonController extends Controller
         }
         return $this->errorResponse($result['message']);
     }
+
+     /**
+     * POST pokemons/round
+     *
+     * Realiza a batalha de um turno entre dois pokemons, um atacante e um defensor
+     *
+     * @bodyParam pokemon:id1 int required id do pokemon. Example: 1
+     * @bodyParam pokemon:id2 int required id do pokemon. Example: 2
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: array<PokemonResource>
+     * }
+     *
+     * @param Request $request Requisição contendo os dados do pokemon
+     * @param int $id1 ID do pokemon atacante
+     * @param int $id2 ID do pokemon defensor
+     * @return JsonResponse
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
 
     public function storeRoundBattle(Request $request): JsonResponse
     {
@@ -96,6 +211,39 @@ class PokemonController extends Controller
         return $this->errorResponse($result['message']);
     }
 
+    /**
+     * PUT /pokemons/{id}
+     *
+     * Atualiza os dados de um pokemon existente
+     *
+     * @urlParam id int required ID do pokemon a ser atualizado. Example: 1
+     *
+     * @bodyParam nome string Nome do pokemon. Example: Caterpie
+     * @bodyParam ataque int Valor de ataque do pokemon. Example: 5
+     * @bodyParam defesa int Valor de defesa do pokemon. Example: 5
+     * @bodyParam vida int Valor total de vida do pokemon. Example: 45
+     * @bodyParam vida_atual int Valor atual de vida do pokemon. Example: 50
+     * @bodyParam tipo string Tipo do pokemon. Example: Inseto
+     * @bodyParam peso string Peso do pokemon. Example: 2.9Kg
+     * @bodyParam localizacao string Localização onde o pokemon pode ser encontrado. Example: Floresta Selvagem - Grama Alta
+     * @bodyParam shiny int Indica se o pokemon é shiny (0 para não, 1 para sim). Example: 0
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string,
+     *
+     * }
+     *
+     * @param PokemonUpdateFormRequest $request Requisição contendo os dados atualizados do pokemon.
+     * @param int $id ID do pokemon a ser atualizado
+     * @return JsonResponse
+     */
 
     public function update(PokemonUpdateFormRequest $request, $id): JsonResponse
     {
@@ -112,6 +260,31 @@ class PokemonController extends Controller
         return $this->errorResponse($result['message']);
     }
 
+      /**
+     * POST pokemons/healing
+     *
+     * Realiza a cura de um pokemon
+     *
+     * @bodyParam pokemon:id int required id do pokemon. Example: 1
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param Request $request Requisição contendo os dados do pokemon
+     * @param int $id ID do pokemon a ser curado
+     * @return JsonResponse
+     *
+     *
+     */
+
     public function storeLife(Request $request): JsonResponse
     {
         $id = $request->input('pokemon:id');
@@ -124,8 +297,30 @@ class PokemonController extends Controller
                 'pokemon' => new PokemonResource($result['data'])
             ]);
         }
-       return $this->errorResponse($result['message']);
+        return $this->errorResponse($result['message']);
     }
+
+    /**
+     * DELETE /pokemons/{id}
+     *
+     * Remove um pokemon existente
+     *
+     * @urlParam id int required ID do pokemon a ser removido. Example: 1
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: PokemonResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param int $id ID do pokemon a ser removido
+     * @return JsonResponse
+     */
 
     public function destroy($id): JsonResponse
     {
