5b1c87ee6b963936066875b10a00f0c17f659274
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-13 17:12:38 -0300

    Adicionando arquivo de test feature

---
+++ tests/Feature/TreinadorFeatureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TreinadorFeatureTest extends TestCase
+{
+
+    public function test_create_treinador_endpoint()
+    {
+
+    }
+}
