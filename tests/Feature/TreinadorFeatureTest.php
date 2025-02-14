2b9cec21c67265d0bb1db4544909128c64ebb553
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-14 19:52:37 -0300

    Adicionando teste de feature para criação de treinador

--- tests/Feature/TreinadorFeatureTest.php
+++ tests/Feature/TreinadorFeatureTest.php
@@ -2,15 +2,45 @@
 
 namespace Tests\Feature;
 
+use App\Models\Pokemon;
+use App\Models\Treinador;
+use App\Services\TreinadorService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TreinadorFeatureTest extends TestCase
 {
+    use RefreshDatabase;
 
+    // php artisan test --filter=TreinadorFeatureTest::test_create_treinador_endpoint
     public function test_create_treinador_endpoint()
     {
+        $pokemons = Pokemon::factory()->count(5)->create();
 
+        $data = [
+            'nome' => 'Treinador Teste',
+            'email' => 'treinador@example.com',
+            'regiao' => 'Unova',
+            'tipo_favorito' => 'Planta',
+            'idade' => 18,
+            'pokemon_id' => $pokemons->random()->id,
+        ];
+
+        $response = $this->postJson('/api/treinadores', $data);
+        $responseData = $response->json('data.0');
+
+        $this->assertDatabaseHas('treinadores', [
+            'email' => 'treinador@example.com',
+        ]);
+        $this->assertModelExists(Treinador::find($responseData['id']));
+        $this->assertEquals($data, [
+            'nome' => $responseData['nome'],
+            'email' => $responseData['email'],
+            'regiao' => $responseData['regiao'],
+            'tipo_favorito' => $responseData['tipo_favorito'],
+            'idade' => $responseData['idade'],
+            'pokemon_id' => $responseData['pokemon_id'],
+        ]);
     }
 }
