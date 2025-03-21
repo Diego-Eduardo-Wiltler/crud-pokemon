c0e319b7fd27d19d3dd0793238adf43f9faf8bba
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-03-21 18:21:55 -0300

    criando form request para troca de pokemons

---
+++ app/Http/Requests/TreinadorTradeRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TreinadorTradeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'treinador:id1' => 'required|exists:treinadores,id|different:treinador:id2',
+            'treinador:id2' => 'required|exists:treinadores,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'different' => 'Os treinadores devem ser diferentes para a troca.'
+        ];
+    }
+}
